a463ccd29bad3fe1d5dff13a1b11cad89f767ba4
v-wenkecheng <v-wenkecheng@wenke1220000>
2023-12-20 07:01:10 +0000

    Add project files.

---
+++ Dev17.6/Anc70_MVCWithRCL_Ind/Anc70_MVCWithRCL_Ind/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Anc70_MVCWithRCL_Ind.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
